85b7105cd6bb468e4d2ebac65faf449f3d274e54
Gregor Best <gbe@ring0.de>
2010-02-18 11:15:58 +0100

    dbg: sort table keys before dumping and add "deep" parameter for nested table dumping
    Signed-off-by: Gregor Best <gbe@ring0.de>

--- dbg.lua
+++ dbg.lua
@@ -8,6 +8,7 @@ local tostring = tostring
 
 local io = io
 local string = string
+local table = table
 
 module("dbg")
 
@@ -21,9 +22,20 @@ function stderr(...)
     io.stderr:flush()
 end
 
-function dump(t, d)
+function keys(t)
+    local k = { }
+    for key, _ in pairs(t) do
+        table.insert(k, key)
+    end
+    table.sort(k)
+    return k
+end
+
+function dump(t, d, deep)
+    deep = (deep ~= nil) or true
     if type(t) ~= "table" then
         stderr("(%s) %s\n", type(t), t)
+        return
     end
 
     d = d or 0
@@ -31,12 +43,16 @@ function dump(t, d)
     if d == 0 then
         stderr("\n")
     end
-    for k, v in pairs(t) do
+    local k = keys(t)
+    for _, k in ipairs(k) do
+        local v = t[k]
         if type(v) == "table" then
             stderr("%s%s (table)\n", p, k)
-            dump(v, d + 1)
+            if deep then
+                dump(v, d + 1, true)
+            end
         else
-            stderr("%s%s (%s) %s\n", p, k, type(v), v)
+            stderr("%s%s (%s) \"%s\"\n", p, k, type(v), v)
         end
     end
 end
